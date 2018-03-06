13faf2fbb8e9145d22ad756615982ebfd27a688d
3llo00osh <ali-lutfi@outlook.com>
2018-03-06 21:51:48 +0800

    Login Done

--- login.js
+++ login.js
@@ -1,3 +1,38 @@
+firebase.auth().onAuthStateChanged(function(user){
+    if (user){ //User is signed in
+      document.getElementById('user-div').style.display="block";
+      document.getElementById('container_demo').style.display="none";
+
+      var user = firebase.auth().currentUser;
+
+      if(user != null){
+        var email_id = user.email;
+
+        document.getElementById('user_para').innerHTML = "Welcome " + email_id;
+
+
+      }
+    }else{
+      //No user signed in
+      document.getElementById('user-div').style.display="none";
+      document.getElementById('container_demo').style.display="block";
+    }
+});
+
 function login(){
-  var uName = document.getElementById("username");
+  var uUsername = document.getElementById("username_field").value;
+  var uPassword = document.getElementById('password_field').value;
+
+  firebase.auth().signInWithEmailAndPassword(uUsername, uPassword).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+
+    window.alert("Error: " + errorMessage);
+  });
+}
+
+function logout(){
+  firebase.auth().signOut();
 }
